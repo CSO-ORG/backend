ed20bc262726795e516e82493d76f685dc8ea67d
KaymKassai <kassai.kaym@gmail.com>
2023-05-30 15:26:23 +0200

    setup api gateway

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.use(json({ limit: '2gb' }));
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: process.env.CURRENT_API_VERSION,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CSOPETFINDER API')
+    .setDescription('API Endpoints')
+    .setVersion(process.env.CURRENT_API_VERSION)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
