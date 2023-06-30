00f345bbdf0d349c675560e1f2f3ed8edc372750
Lilian-MMI <lilianouvmmi@gmail.com>
2023-06-30 18:48:19 +0200

    feat: logger console + save it to .log file

--- pet-alert-scrapper/src/index.ts
+++ pet-alert-scrapper/src/index.ts
@@ -4,6 +4,7 @@ import {
 	getNumberOfPagesFromAlertNumber,
 	getPetAlertsDepartementsLinks,
 	invokeWorker,
+	logger,
 	mergeFiles,
 	sendFilesToGateway,
 } from '@services/index';
@@ -16,89 +17,108 @@ import 'reflect-metadata';
 
 const activeWorkers = new Set<string>();
 
-console.log('Start scraping');
+try {
+	logger.info('Start scraping');
 
-const $ = cheerio.load(
-	await axios(CONFIG.URL_TO_SCRAPE).then((res) => res.data),
-);
-
-const petAlertsDepartementsLinks = getPetAlertsDepartementsLinks($);
-
-const start = performance.now();
-const petAlertsLinksUnvisited = petAlertsDepartementsLinks;
-
-while (petAlertsLinksUnvisited.length > 0) {
-	if (activeWorkers.size >= CONFIG.MAX_WORKERS) {
-		await delay(1000);
-		continue;
-	}
-
-	console.log(
-		`Remaining ${CONFIG.MAX_WORKERS - activeWorkers.size} workers available`,
+	const $ = cheerio.load(
+		await axios(CONFIG.URL_TO_SCRAPE).then((res) => res.data),
 	);
 
-	for (let i = 0; i < CONFIG.MAX_WORKERS - activeWorkers.size; i++) {
-		let currentWorker: string | undefined;
+	const petAlertsDepartementsLinks = getPetAlertsDepartementsLinks($);
 
-		for (const workerName of WORKERS_NAME) {
-			if (!activeWorkers.has(workerName)) {
-				currentWorker = workerName;
-				activeWorkers.add(workerName);
-				break;
-			}
+	const start = performance.now();
+	const petAlertsLinksUnvisited = petAlertsDepartementsLinks;
+
+	while (petAlertsLinksUnvisited.length || activeWorkers.size > 0) {
+		if (activeWorkers.size >= CONFIG.MAX_WORKERS) {
+			await delay(1000);
+			continue;
 		}
 
-		if (!currentWorker) continue;
+		logger.debug(activeWorkers.size.toString());
 
-		console.log(`[START] - [worker - ${currentWorker}]`);
+		logger.info(
+			`Remaining ${
+				CONFIG.MAX_WORKERS - activeWorkers.size
+			} workers available(s)`,
+		);
 
-		const url = petAlertsDepartementsLinks[petAlertsLinksUnvisited.length - 1];
-		petAlertsLinksUnvisited.pop();
+		for (let i = 0; i < CONFIG.MAX_WORKERS - activeWorkers.size; i++) {
+			const url =
+				petAlertsDepartementsLinks[petAlertsLinksUnvisited.length - 1];
+			petAlertsLinksUnvisited.pop();
 
-		if (!url) continue;
-
-		const $ = cheerio.load(await axios(url).then((res) => res.data));
-		const numberOfAlerts = getNumberOfAlert($);
-		const sitePages = getNumberOfPagesFromAlertNumber(numberOfAlerts);
-
-		const { code, name } = getCodeAndNameFromDepartement(url);
-
-		const worker = invokeWorker(code, name, sitePages, currentWorker, i);
-
-		worker.on('error', (err: unknown) => {
-			if (err instanceof Error && err.message === 'Gateway Timeout') {
-				petAlertsLinksUnvisited.push(url);
-				activeWorkers.delete(currentWorker as string);
-				console.log(
-					`[STOP] [worker - ${currentWorker}] is done. Gateway Timeout ! Retry incoming...`,
+			if (!url) {
+				logger.info(
+					'[STOP] No more links to visit. Waiting for workers to finish...',
 				);
+				await delay(5000);
+				continue;
 			}
-			console.log(err);
-		});
 
-		worker.on('message', (workerName: string) => {
-			activeWorkers.delete(workerName);
-			console.log(
-				`[STOP] [worker - ${workerName}] is done. Get ${sitePages} pages with ${numberOfAlerts} alerts.`,
+			let currentWorker: string | undefined;
+
+			for (const workerName of WORKERS_NAME) {
+				if (!activeWorkers.has(workerName)) {
+					currentWorker = workerName;
+					activeWorkers.add(workerName);
+					break;
+				}
+			}
+
+			logger.info(
+				`[START] - [worker - ${currentWorker}] on ${url.split('/').pop()}`,
 			);
-		});
+
+			const $ = cheerio.load(await axios(url).then((res) => res.data));
+			const numberOfAlerts = getNumberOfAlert($);
+			const sitePages = getNumberOfPagesFromAlertNumber(numberOfAlerts);
+
+			const { code, name } = getCodeAndNameFromDepartement(url);
+
+			const worker = invokeWorker(
+				code,
+				name,
+				sitePages,
+				currentWorker as string,
+				i,
+			);
+
+			worker.on('error', (err: unknown) => {
+				if (err instanceof Error && err.message === 'Gateway Timeout') {
+					petAlertsLinksUnvisited.push(url);
+					activeWorkers.delete(currentWorker as string);
+					logger.error(
+						`[STOP] [worker - ${currentWorker} on ${url
+							.split('/')
+							.pop()}] KO. Gateway Timeout ! Retry incoming...`,
+					);
+				}
+			});
+
+			worker.on('message', (workerName: string) => {
+				activeWorkers.delete(workerName);
+				logger.info(
+					`[STOP] [worker - ${workerName} on ${url
+						.split('/')
+						.pop()}] is done. Get ${sitePages} pages with ${numberOfAlerts} alerts.`,
+				);
+			});
+		}
 	}
+
+	logger.info('All workers are done');
+
+	const end = performance.now();
+	logger.info(`Execution time: ${((end - start) / 1000).toFixed(2)} seconds`);
+
+	logger.info('Start merging files');
+	mergeFiles();
+	logger.info('All files are merged');
+
+	logger.info('Start sending files to gateway');
+	sendFilesToGateway();
+	logger.info('All files are sent to gateway');
+} catch (err) {
+	logger.error(err);
 }
-
-while (activeWorkers.size > 0) {
-	console.log(`Waiting for ${activeWorkers.size} workers to finish their job`);
-	await delay(1000);
-}
-
-console.log('All workers are done');
-
-const end = performance.now();
-console.log(`Execution time: ${((end - start) / 1000).toFixed(2)} seconds`);
-
-console.log('Start merging files');
-mergeFiles();
-
-console.log('All files are merged');
-sendFilesToGateway();
-
-console.log('All files are sent to gateway');
