b96bf42af4700d3fb3efc869008413e2cd0fb959
lilian_ouvrard <Lilian_OUVRARD@connect-tech.sncf>
2023-09-22 10:56:31 +0200

    fix: remove logger

--- pet-alert-scrapper/src/services/logger.ts
+++ pet-alert-scrapper/src/services/logger.ts
@@ -1,50 +1,16 @@
 import pino from 'pino';
 
-const transport = pino.transport({
-	targets: [
-		{
-			level: 'trace',
-			target: 'pino/file',
-			options: {
-				ignore: 'pid,hostname',
-				destination: './logs/trace.log',
-			},
-		},
-		{
-			level: 'info',
-			target: 'pino/file',
-			options: {
-				ignore: 'pid,hostname',
-				destination: './logs/info.log',
-			},
-		},
-		{
-			level: 'error',
-			target: 'pino/file',
-			options: {
-				ignore: 'pid,hostname',
-				destination: './logs/error.log',
-			},
-		},
-		{
-			level: 'info',
-			target: 'pino-pretty',
-			options: {
-				ignore: 'pid,hostname',
-			},
-		},
-	],
-
-	dedupe: true,
-});
-
 /**
  * Logger instance (pino)
  */
-export const logger = pino(
-	{
-		level: 'trace',
-		timestamp: () => `,"time":"${new Date().toISOString()}"`,
+export const logger = pino({
+	level: 'trace',
+	timestamp: () => `,"time":"${new Date().toISOString()}"`,
+	transport: {
+		target: 'pino-pretty',
+		options: {
+			colorize: true,
+			ignore: 'pid,hostname',
+		},
 	},
-	transport,
-);
+});
