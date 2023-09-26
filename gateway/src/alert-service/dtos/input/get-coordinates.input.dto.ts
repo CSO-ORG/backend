553ac100d368d73755a6cddcb19a177c20c9f463
KaymKassai <kassai.kaym@gmail.com>
2023-09-26 17:34:47 +0200

    fix: apply filters to coords

--- gateway/src/alert-service/dtos/input/get-coordinates.input.dto.ts
+++ gateway/src/alert-service/dtos/input/get-coordinates.input.dto.ts
@@ -8,6 +8,12 @@ export class GetCoordinatesInputDto {
     description: 'list of options',
     default: {
       alertType: ALERT_CATEGORY.LOST_PET,
+      breeds: ['breed 1', 'breed 2'],
+      suspicious: false,
+      cities: ['Nantes', 'Paris'],
+      departmentCodes: ['44', '77', '22'],
+      species: ['specie 1', 'specie 2'],
+      rewardRange: [100, 120],
     },
   })
   @IsValidGetAlertsFilters({ context: { type: 'getAll' } })
